7aa958f8426b4ed9cc245d48bf641cba11d188a9
S. V. Paulauskas <stanpaulauskas@gmail.com>
2020-10-27 16:12:53 -0400

    Checkpointing work

--- data_investigation.py
+++ data_investigation.py
@@ -1,8 +1,11 @@
 """
 Data investigation
 """
+from uuid import uuid4
+from os import listdir
 
-from pandas import read_csv, set_option
+from pandas import read_csv, set_option, DataFrame
+from sklearn import decomposition
 
 set_option('display.max_columns', None)
 
@@ -11,22 +14,26 @@ DATA_LABELS = ['unit', 'cycles', 'op_setting1', 'op_setting2', 'op_setting3', 's
                's17', 's18', 's19', 's20', 's21']
 
 
-def generate_data_set_name(dataset_type, dataset_number):
-    return f'Data/{dataset_type}_FD00{dataset_number}.txt'
-
-
 def main():
+    data_base_dir = "Data"
     dataset_name = 'train'
-    dataset_number = 1
-    df = read_csv(generate_data_set_name(dataset_name, dataset_number),
-                  delimiter=" ", names=DATA_LABELS, usecols=range(len(DATA_LABELS)))
+
+    df = DataFrame()
+    for file in [f for f in listdir(data_base_dir) if f.find(dataset_name) != -1]:
+        tmp_df = read_csv(f"{data_base_dir}/{file}", delimiter=" ", names=DATA_LABELS,
+                          usecols=range(len(DATA_LABELS)))
+        for i in range(tmp_df.unit.max()):
+            tmp_df.map()
+        print(df)
+        # df = df.append()
+    # print(df.describe())
 
     is_last_measurement_index = df.groupby(['unit'])['cycles'].transform(max) == df['cycles']
     last_measured_df = df[is_last_measurement_index]
-    print(last_measured_df)
-
     penultimate_measured_df = df[df.index.isin([x - 1 for x in last_measured_df.index])]
-    print(penultimate_measured_df)
+
+    last_measured_df.to_csv("Data/temp-failure.csv")
+    penultimate_measured_df.to_csv("Data/temp-before-failure.csv")
 
 
 if __name__ == '__main__':
